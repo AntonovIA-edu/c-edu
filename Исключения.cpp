0a67d477320b5b15df0d44f95ccf5b4ea1e0d3c5
AntonovIA-edu <119353493+AntonovIA-edu@users.noreply.github.com>
2022-12-26 11:05:26 +0300

    Update Исключения.cpp

--- Исключения.cpp
+++ Исключения.cpp
@@ -1,56 +1,107 @@
 #include <iostream>
-#include <algorithm>
-#include <math.h>
+#include <stdexcept>
+#include <string>
 
 using namespace std;
-const char* msg;
-const bool* error;
 
-int root_of_equation_d(const int& a, const int& b, const int& c)
-{
-    int d = b * b - 4 * a * c;
-    if (d < 0)
-        throw msg = "Дискриминант меньше 0!";
-    return((b * -1) + d) / (2 * a);
-    
-}
-void equation(const int& a)
-{
-    
-    if (a == 0)
-        throw error = 0;
-    
-}
+class MyExcept1 {}; 
 
-int main()
-{
-    setlocale(LC_ALL, "rus");
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    cout << "Введите a =  ";
-    cin >> a;
-    cout << "\n\nВведите b =  ";
-    cin >> b;
-    cout << "\n\nВведите c =  ";
-    cin >> c;
+class MyExcept2 { 
+    string msg;
+public:
+    MyExcept2(const string& s) : msg(s) {}
+    const char* message() const { return msg.c_str(); }
+};
 
-    try
-    {
-        equation(a);
-        cout<<"\n\nКорень уравнения = "<<root_of_equation_d(a, b, c)<<"\n\n";
-    }
-    catch (const bool* error)
-    {
-        cout << "\n\nОшибка! " <<  "A равно 0!" << "\n" << endl;
-        cout << "Конец работы программы!\n";
-        return 1;
-    }
-    catch (const char* msg)
-    {
-        cout << "\n\nОшибка! " << msg << "\n" << endl;
-    }
-    cout << "Конец работы программы!\n";
-    return 1;
+class MyExcept3 : public invalid_argument { 
+    double num;
+public:
+    MyExcept3(const string msg, double n) : invalid_argument(msg), num(n) {}
+    double arg() const { return num; }
+};
+
+bool is_leapYear1(int year) {
+    if (year <= 0) throw "Неправильный год";
+    return (!(year % 4) && (year % 100)) || (!(year % 400));
 }
 
+bool is_leapYear2(int year) throw() {
+    if (year <= 0) throw "Неправильный год";
+    return (!(year % 4) && (year % 100)) || (!(year % 400));
+}
+
+bool is_leapYear3(int year) throw(invalid_argument) {
+    if (year <= 0)
+        throw invalid_argument("Неправильный год");
+    return (!(year % 4) && (year % 100)) || (!(year % 400));
+}
+bool is_leapYear4(int year) throw(MyExcept1) {
+    if (year <= 0)
+        throw MyExcept1();
+    return (!(year % 4) && (year % 100)) || (!(year % 400));
+}
+
+bool is_leapYear5(int year) throw(MyExcept2) {
+    if (year <= 0)
+        throw MyExcept2("Неправильный год");
+    return (!(year % 4) && (year % 100)) || (!(year % 400));
+}
+
+bool is_leapYear6(int year) throw(MyExcept3) {
+    if (year <= 0)
+        throw MyExcept3("Неправильный год", year);
+    return (!(year % 4) && (year % 100)) || (!(year % 400));
+}
+
+void launcher(bool (*func)(int)) {
+    int year;
+    cout << "Введите год >> ";
+    cin >> year;
+    cout << "Високосный год? " << func(year) << " (0 - Нет, 1 - Да)" << endl;
+}
+
+int main() {
+    cout << "Функция определяет, является ли год високосным годом. Введите год" << endl;
+
+    try {
+        launcher(is_leapYear1);
+    }
+    catch (...) {
+        cout << "В функции is_leapYear1 произошло исключение" << endl;
+    }
+
+    try {
+        launcher(is_leapYear2);
+    }
+    catch (...) {
+        cout << "В функции is_leapYear2 произошло исключение" << endl;
+    }
+
+    try {
+        launcher(is_leapYear3);
+    }
+    catch (invalid_argument& e) {
+        cout << e.what() << endl;
+    }
+
+    try {
+        launcher(is_leapYear4);
+    }
+    catch (MyExcept1) {
+        cout << "В функции is_leapYear4 произошло исключение" << endl;
+    }
+
+    try {
+        launcher(is_leapYear5);
+    }
+    catch (MyExcept2& e) {
+        cout << e.message() << endl;
+    }
+
+    try {
+        launcher(is_leapYear6);
+    }
+    catch (MyExcept3& e) {
+        cout << e.what() << " " << e.arg() << endl;
+    }
+}
